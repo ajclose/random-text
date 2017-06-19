d4201393bc2756e4f3f928cd47f802ae7c17a94b
Alex Close <ajclose@gmail.com>
2017-06-19 13:40:42 -0500

    Add html tags to script

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ const loremIpsum = require('lorem-ipsum')
 const app = express();
 
 app.get('/lorem', function (req, res) {
-  res.send(loremIpsum({
+  res.send(`<html><head><title>Lorem Ipsum</title </head><body>${loremIpsum({
     count: 3,
     units: 'paragraphs',
     format: 'html'
-  }));
+  })}  </body></html>`);
 });
 
 app.get('/lorem/:count', function (req, res) {
-  res.send(loremIpsum({
+  res.send(`<html><head><title>Lorem Ipsum</title </head><body>${loremIpsum({
     count: req.params.count,
     units: 'paragraphs',
     format: 'html'
-  }));
+  })}  </body></html>`);
 });
+
 
 app.listen(3000, function () {
   console.log('Successfully started express application!')
